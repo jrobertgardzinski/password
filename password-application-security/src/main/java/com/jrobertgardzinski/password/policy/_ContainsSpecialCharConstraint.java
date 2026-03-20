47736de0937a801520543d7086c284ea1fd41c7a
Robert Gardziński <jrobertgardzinski@gmail.com>
2026-03-20 09:27:13 +0100

    Move password lib to a separate repository

---
+++ password-application-security/src/main/java/com/jrobertgardzinski/password/policy/_ContainsSpecialCharConstraint.java
@@ -0,0 +1,27 @@
+package com.jrobertgardzinski.password.policy;
+
+import com.jrobertgardzinski.password.domain.PlaintextPassword;
+import com.jrobertgardzinski.util.constraint.ErrorConstraint;
+
+import java.util.regex.Pattern;
+
+class _ContainsSpecialCharConstraint extends ErrorConstraint<PlaintextPassword> {
+
+    private final String specialChars;
+    private final Pattern pattern;
+
+    _ContainsSpecialCharConstraint(String specialChars) {
+        this.specialChars = specialChars;
+        this.pattern = Pattern.compile("[" + Pattern.quote(specialChars) + "]");
+    }
+
+    @Override
+    public boolean isSatisfied(PlaintextPassword candidate) {
+        return pattern.matcher(candidate.value()).find();
+    }
+
+    @Override
+    public String code() {
+        return "SPECIAL_CHAR_REQUIRED";
+    }
+}
---
+++ password-application-security/src/main/java/com/jrobertgardzinski/password/policy/_ContainsUppercaseConstraint.java
@@ -0,0 +1,21 @@
+package com.jrobertgardzinski.password.policy;
+
+import com.jrobertgardzinski.password.domain.PlaintextPassword;
+import com.jrobertgardzinski.util.constraint.ErrorConstraint;
+
+import java.util.regex.Pattern;
+
+class _ContainsUppercaseConstraint extends ErrorConstraint<PlaintextPassword> {
+
+    private static final Pattern UPPERCASE = Pattern.compile("[A-Z]");
+
+    @Override
+    public boolean isSatisfied(PlaintextPassword candidate) {
+        return UPPERCASE.matcher(candidate.value()).find();
+    }
+
+    @Override
+    public String code() {
+        return "UPPERCASE_REQUIRED";
+    }
+}
