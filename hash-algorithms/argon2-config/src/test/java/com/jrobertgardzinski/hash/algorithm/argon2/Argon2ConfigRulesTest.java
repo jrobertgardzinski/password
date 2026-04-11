5ae87bb1ef2688fddb7ec1276be6511a16455524
Robert Gardziński <jrobertgardzinski@gmail.com>
2026-04-11 07:22:14 +0200

    Remove the "Loads parameters from application.properties" test since it's a wrong place for it.

--- hash-algorithms/argon2-config/src/test/java/com/jrobertgardzinski/hash/algorithm/argon2/Argon2ConfigRulesTest.java
+++ hash-algorithms/argon2-config/src/test/java/com/jrobertgardzinski/hash/algorithm/argon2/Argon2ConfigRulesTest.java
@@ -13,24 +13,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Feature("Argon2 Configuration")
 class Argon2ConfigRulesTest {
 
-    @Example
-    @Label("Loads parameters from application.properties")
-    void loadsFromProperties() {
-        Map<String, String> props = Map.of(
-            "argon2.iterations", "10",
-            "argon2.memLimit", "32768",
-            "argon2.parallelism", "2"
-        );
-
-        Argon2Config config = Argon2Config.from(props::get);
-
-        assertThat(config.iterations().value()).isEqualTo(10);
-        assertThat(config.memLimit().value()).isEqualTo(32768);
-        assertThat(config.parallelism().value()).isEqualTo(2);
-    }
-
     @Property
-    @Label("Applies OWASP defaults when properties are absent")
+    @Label("Applies defaults when properties are absent")
     void appliesDefaultsWhenPropertiesAbsent() {
         assertThat(Argon2Config.builder().build()).isEqualTo(Argon2Config.withDefaults());
     }
