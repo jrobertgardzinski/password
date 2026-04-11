b1572284e92f05690e38e1e3b5b8f87c1938eec5
Robert Gardziński <jrobertgardzinski@gmail.com>
2026-04-11 07:56:17 +0200

    Refactor withDefaults test

--- hash-algorithms/argon2-config/src/main/java/com/jrobertgardzinski/hash/algorithm/argon2/MemLimitInKB.java
+++ hash-algorithms/argon2-config/src/main/java/com/jrobertgardzinski/hash/algorithm/argon2/MemLimitInKB.java
@@ -4,6 +4,7 @@ public record MemLimitInKB(int value) {
 
     public static final int MIN = 8;
     public static final int MAX = 4_194_304;
+    public static final int DEFAULT = 65_536;
 
     public MemLimitInKB {
         if (value < MIN) throw new IllegalArgumentException("MemLimitInKB cannot be less than: " + MIN);
